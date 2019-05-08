5bbbef609ee550877b320be43d4e8cc0bfe28705
Smart Developer <smartdeveloper0124@gmail.com>
2019-05-08 09:02:21 -0700

    Simple Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
+
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Link, NavLink, Switch} from 'react-router-dom'
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import Header from 'pages/header'
+
+// import Home from 'pages/home'
+// import Signup from 'pages/Signup'
+// import Login from 'pages/Login'
+
+export interface Props {
+
 }
+
+class App extends React.Component<Props, object>{
+
+  constructor(props: Props) {
+    super(props);
+  } 
+
+  render() {
+    const auth = true;
+    return (
+        
+        <Router>
+          <Header></Header>
+
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/login" component={Login}/>
+            <Route path="/signup" component={Signup}/>
+          </Switch>
+
+        </Router>
+    )
+  }
+}
+
+const Header = () => {
+
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login">Login</NavLink>
+        </li>
+        <li>
+          <NavLink to="/signup">Signup</NavLink>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+const Home = () => <div>Home</div>
+
+const Login = () => <div>Login</div>
+
+const Signup = () => <div>Signup</div>
+
+
 
 export default App;
